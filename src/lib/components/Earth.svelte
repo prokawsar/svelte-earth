f8aaf02b9139c2ed6044848e4aa46e24f3174d97
Kawsar Ahmed <prokawsar@gmail.com>
2023-11-12 09:47:05 -0500

    format and methods

--- src/lib/components/Earth.svelte
+++ src/lib/components/Earth.svelte
@@ -1,57 +1,66 @@
 <script lang="ts">
-	import { sceneStore } from '$lib/store';
-	import { onDestroy, onMount } from 'svelte';
-	import * as THREE from 'three';
-	import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+	import { sceneStore } from '$lib/store'
+	import { onDestroy, onMount } from 'svelte'
+	import * as THREE from 'three'
+	import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-	let earthcanvas: HTMLCanvasElement;
-	let cameraZoom = 15; // Initial camera zoom
-	let camera: THREE.PerspectiveCamera, renderer: THREE.Renderer, scene: THREE.Scene;
+	let earthcanvas: HTMLCanvasElement
+	let cameraZoom = 15 // Initial camera zoom
+	let camera: THREE.PerspectiveCamera, renderer: THREE.Renderer, scene: THREE.Scene
+
+	const initVariables = () => {
+		// Set up scene, camera, and renderer
+		scene = new THREE.Scene()
+		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100)
+		renderer = new THREE.WebGLRenderer({ alpha: false, canvas: earthcanvas })
+		renderer.setSize(window.innerWidth, window.innerHeight)
+	}
 
 	const onWindowResize = () => {
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-		renderer.setSize(window.innerWidth, window.innerHeight);
-	};
+		camera.aspect = window.innerWidth / window.innerHeight
+		camera.updateProjectionMatrix()
+		renderer.setSize(window.innerWidth, window.innerHeight)
+	}
+
+	const setControls = () => {
+		const controls = new OrbitControls(camera, renderer.domElement)
+		controls.rotateSpeed = 0.4
+		controls.update()
+	}
 
 	onMount(() => {
-		// Set up scene, camera, and renderer
-		scene = new THREE.Scene();
-		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100);
-		renderer = new THREE.WebGLRenderer({ alpha: false, canvas: earthcanvas });
-		renderer.setSize(window.innerWidth, window.innerHeight);
+		initVariables()
+		setEarthGeo()
+		setControls()
+		setCamera()
+		animate()
 
+		window.addEventListener('resize', onWindowResize, false)
+		sceneStore.set(scene)
+	})
+
+	const setEarthGeo = () => {
 		// Create Earth geometry and texture
-		const geometry = new THREE.SphereGeometry(8, 80, 80);
-		const textureLoader = new THREE.TextureLoader();
-		const earthTexture = textureLoader.load('/images/earthmap4k.jpg'); // Replace with the path to your texture
-		const material = new THREE.MeshBasicMaterial({ map: earthTexture });
-		const earth = new THREE.Mesh(geometry, material);
-		scene.add(earth);
+		const geometry = new THREE.SphereGeometry(8, 80, 80)
+		const textureLoader = new THREE.TextureLoader()
+		const earthTexture = textureLoader.load('/images/earthmap4k.jpg') // Replace with the path to your texture
+		const material = new THREE.MeshBasicMaterial({ map: earthTexture })
+		const earth = new THREE.Mesh(geometry, material)
+		scene.add(earth)
+	}
 
-		const controls = new OrbitControls(camera, renderer.domElement);
-		controls.rotateSpeed = 0.4;
-		controls.update();
-
+	const setCamera = () => {
 		// Set camera position
-		camera.position.z = cameraZoom;
-		camera.position.x = 0;
-		camera.position.y = 0;
+		camera.position.z = cameraZoom
+		camera.position.x = 0
+		camera.position.y = 0
+	}
 
-		window.addEventListener('resize', onWindowResize, false);
-
-		// Animation function
-		const animate = () => {
-			requestAnimationFrame(animate);
-			// Rotate the Earth
-			// earth.rotation.y += 0.001;
-
-			renderer.render(scene, camera);
-		};
-
-		animate();
-		sceneStore.set(scene);
-	});
+	// Animation function
+	const animate = () => {
+		requestAnimationFrame(animate)
+		renderer.render(scene, camera)
+	}
 </script>
 
 <canvas bind:this={earthcanvas} />
